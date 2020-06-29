eca6da185ae20792920448dbeebe7d3b08697b47
Pritam Ghosh <meghoshpritam@gmail.com>
2020-06-30 02:56:17 +0530

    :fire:

--- src/routes/transaction.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import TransactionController from '../controllers/TransactionController';
-
-const routes: Router = Router();
-
-routes.get('/payment-capture', TransactionController.createOrder);
-
-export default routes;
