4dedc702888d4be052e5014b2eb02caf53d945c2
Pritam Ghosh <meghoshpritam@gmail.com>
2020-07-18 10:02:48 +0530

    :construction: updated

--- src/controllers/pageController/adminPageController.ts
+++ src/controllers/pageController/adminPageController.ts
@@ -1,3 +1,6 @@
+import Users, { User } from './../../entity/Users';
+import Assignments, { Assignment } from './../../entity/Assignments';
+import Exams, { Exam } from './../../entity/Exams';
 import Projects, { Project } from './../../entity/Projects';
 import Courses, { Course } from './../../entity/Courses';
 import { Request, Response, NextFunction } from 'express';
@@ -9,7 +12,8 @@ class AdminPageController {
       // all nodes
       const nodes: Node[] = await Nodes.find({ creator: res.locals.id })
         .populate({ path: 'creator', select: 'name' })
-        .select('name description img video updated creator type');
+        .select('name description img video updated creator type')
+        .exec();
 
       // all courses
       const courses: Course[] = await Courses.find({ creator: res.locals.id })
@@ -17,7 +21,8 @@ class AdminPageController {
           path: 'creator',
           select: 'name',
         })
-        .select('name description price creator img video updated');
+        .select('name description price creator img video updated')
+        .exec();
 
       // all projects
       const projects: Project[] = await Projects.find({ creator: res.locals.id })
@@ -25,7 +30,8 @@ class AdminPageController {
           path: 'creator',
           select: 'name',
         })
-        .select('name description price creator img video updated');
+        .select('name description price creator img video updated')
+        .exec();
 
       // all goals
       const goals: Project[] = await Projects.find({ creator: res.locals.id })
@@ -33,11 +39,34 @@ class AdminPageController {
           path: 'creator',
           select: 'name',
         })
-        .select('name description price creator img video updated');
+        .select('name description price creator img video updated')
+        .exec();
+
+      // all exams
+      const exams: Exam[] = await Exams.find({ creator: res.locals.id })
+        .populate({
+          path: 'creator',
+          select: 'name',
+        })
+        .exec();
+
+      // all assignments
+      const assignments: Assignment[] = await Assignments.find({ creator: res.locals.id })
+        .populate({ path: 'creator', select: 'name' })
+        .exec();
 
       // fetch the ratings
 
-      res.status(200).json({ nodes, courses, projects, goals });
+      if (res.locals.type === 'admin') {
+        const instructors: User[] = await Users.find({ type: 'instructor' })
+          .select('name active inactiveMsg role join about avatar')
+          .exec();
+
+        res.status(200).json({ nodes, courses, projects, goals, exams, assignments, instructors });
+        return;
+      }
+
+      res.status(200).json({ nodes, courses, projects, goals, exams, assignments });
     } catch (err) {
       next(err);
     }
